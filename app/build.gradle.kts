350a27f2259ae004c2f8774a33619780ba839aa3
Vanessa Vo <vanessa13570@gmail.com>
2023-12-06 12:27:11 -0500

    testing build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("org.mindrot:jbcrypt:0.4")
     testImplementation("junit:junit:4.13.2")
+    testImplementation ("org.mockito:mockito-core:3.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
